f2eee7204f9526cf591f14fd9072459b67ba35d9
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-19 15:15:27 -0500

    playing with break and continue

--- for.go
+++ for.go
@@ -8,9 +8,8 @@ import (
 func main() {
 
 	for timer := 10; timer >= 0; timer-- {
-		if timer == 0 {
-			fmt.Println("Boom!")
-			break
+		if timer%2 == 0 {
+			continue
 		}
 		fmt.Println(timer)
 		time.Sleep(1 * time.Second)
