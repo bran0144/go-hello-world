8d93046e95a5872a74bd334ada32a746ccdbc7b5
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-19 17:23:29 -0500

    append

--- slices.go
+++ slices.go
@@ -5,11 +5,14 @@ import (
 )
 
 func main() {
-	mySlice := make([]int, 1, 4)
-	fmt.Printf("Length is %d Capacity is: %d", len(mySlice), cap(mySlice))
+	mySlice := []int{1, 2, 3, 4, 5}
+	fmt.Println(mySlice)
 
-	for i := 1; i < 17; i++ {
-		mySlice = append(mySlice, i)
-		fmt.Printf("\nCapacity is: %d", cap(mySlice))
+	for _, i := range mySlice {
+		fmt.Println("for range loop:", i)
 	}
+
+	newSlice := []int{10, 20, 30}
+	mySlice = append(mySlice, newSlice...)
+	fmt.Println(mySlice)
 }
