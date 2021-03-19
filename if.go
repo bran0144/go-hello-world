2fe828685354f0f6e3d442efeebccbb0ed83a884
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-19 14:04:31 -0500

    nested if statements

--- if.go
+++ if.go
@@ -8,8 +8,14 @@ func main() {
 
 	if firstRank, secondRank := 39, 614; firstRank < secondRank {
 		fmt.Println("\nFirst course is doing better" + " than second course")
+		if firstRank > 100 {
+			fmt.Println("You may want to consider another job")
+		}
 	} else if firstRank > secondRank {
-		fmt.Println("\nYour first" + "course must be doing abysmally!")
+		fmt.Println("\nYour first course must be doing abysmally!")
+		if secondRank > 100 {
+			fmt.Println("You may wanna consider another job")
+		}
 	} else {
 		fmt.Println("\nBoth courses are doing the same")
 	}
