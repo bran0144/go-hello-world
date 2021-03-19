0a12f4ad22612a55121e4d4b25bb1ac8cbf3c18b
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-19 12:09:51 -0500

    passing by reference works

--- variable-practice.go
+++ variable-practice.go
@@ -18,13 +18,13 @@ func main() {
 
 	fmt.Println("\nHi", name, "you're currently watching", course)
 
-	changeCourse(course)
+	changeCourse(&course)
 
 	fmt.Println("\nYou are now watching course", course)
 }
 
-func changeCourse(course string) string {
-	course = "First look: Native Docker Clustering"
-	fmt.Println("\nTrying to change your course to", course)
-	return course
+func changeCourse(course *string) string {
+	*course = "First look: Native Docker Clustering"
+	fmt.Println("\nTrying to change your course to", *course)
+	return *course
 }
