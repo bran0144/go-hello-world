131652c9008ac4502be9c644dcc096e94770637c
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-19 17:07:45 -0500

    slices of slice

--- slices.go
+++ slices.go
@@ -5,7 +5,12 @@ import (
 )
 
 func main() {
-	myCourses := []string{"Docker", "Puppet", "Python"}
+	mySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	fmt.Printf("Length is %d. \nCapacity is: %d", len(myCourses), cap(myCourses))
+	fmt.Println(mySlice[4])
+	mySlice[1] = 0
+	fmt.Println(mySlice)
+
+	sliceOfSlice := mySlice[2:5]
+	fmt.Println(sliceOfSlice)
 }
