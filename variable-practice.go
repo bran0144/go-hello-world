20e08d00f61beb0b38ae48491627f9a019fadcdc
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-19 09:51:38 -0500

    declaring at function  level

--- variable-practice.go
+++ variable-practice.go
@@ -5,15 +5,20 @@ import (
 	"reflect"
 )
 
-var (
-	// name, course string
-	// module       float64
-	name   = "Nigel"            //Name of subscriber
-	course = "Docker Deep Dive" //Name of course
-	module = 3.2                //Current module
-)
+// var (
+// 	// name, course string
+// 	// // module       float64
+// 	name   = "Nigel"            //Name of subscriber
+// 	course = "Docker Deep Dive" //Name of course
+// 	module = 3.2                //Current module
+// )
 
 func main() {
+	name := "Nigel"
+	// course := "Docker Deep Dive"
+	module := 3.2
+
 	fmt.Println("Name is set to", name, "and is of type", reflect.TypeOf(name))
 	fmt.Println("Module is set to", module, "and is of type", reflect.TypeOf(module))
+
 }
