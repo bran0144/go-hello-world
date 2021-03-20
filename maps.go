cf06c4ab2c71b954776e8fcdaa0adc7bcd05bf70
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-20 16:33:49 -0500

    playing with maps

--- maps.go
+++ maps.go
@@ -7,7 +7,5 @@ import (
 func main() {
 	testMap := map[string]int{"A": 1, "B": 2, "C": 3, "D": 4, "E": 5}
 
-	for key, value := range testMap {
-		fmt.Printf("\nKey is: %v Value is: %v", key, value)
-	}
+	fmt.Println(testMap["C"])
 }
