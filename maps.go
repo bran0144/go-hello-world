3347975a8f0afb223eea5c183e3208709954294e
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-20 17:08:05 -0500

    structs

--- maps.go
+++ maps.go
@@ -8,5 +8,9 @@ func main() {
 	testMap := map[string]int{"A": 1, "B": 2, "C": 3, "D": 4, "E": 5}
 
 	testMap["A"] = 100
+	testMap["F"] = 197
+	fmt.Println(testMap)
+
+	delete{testMap, "F"}
 	fmt.Println(testMap)
 }
