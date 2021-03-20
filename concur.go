4e4a9e92347e207b58be2d2603eec13f0c40dc89
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-20 17:34:13 -0500

    added concurrency

--- concur.go
+++ concur.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	func() {
+
+	var waitGrp sync.WaitGroup
+	waitGrp.Add(2)
+
+	go func() {
+		defer waitGrp.Done()
+
 		time.Sleep(5 * time.Second)
 		fmt.Println("Hello")
 	}()
 
-	func() {
+	go func() {
+		defer waitGrp.Done()
+
 		fmt.Println("Pluralsight")
 	}()
+
+	waitGrp.Wait()
 }
